26009726e393d36c31f4df31926648fcbf788bae
fredafei <964959033@qq.com>
2020-07-04 16:53:33 +0800

    获取当前用户信息

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   def create
     render_resource User.create create_params
   end
-
+  def me
+    render_resource current_user
+  end
   def create_params
     params.permit(:email, :password, :password_confirmation)
   end
