19a46191428d21f4c5de8b5d926b8d2ac83c7eb2
fredafei <964959033@qq.com>
2020-07-04 15:33:41 +0800

    创建user & 验证

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+  def create
+    render_resource User.create create_params
+  end
+
+  def create_params
+    params.permit(:email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_format_of :email, with: /.+@.+/, if: Proc.new { |u| u.email.present? }
+
+  validates :password, length: {minimum: 6, maximum: 8}, on: [:create], if: :password
+  validates_presence_of :password_confirmation, on: [:create]
+
+
 end
