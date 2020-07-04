7bffb75412cb09f7f5e2d65d13f366cc37ccf9d5
fredafei <964959033@qq.com>
2020-07-04 16:46:27 +0800

    添加登录&校验

---
+++ app/models/session.rb
@@ -0,0 +1,25 @@
+class Session
+  include ActiveModel::Model
+  attr_accessor :email, :password, :user
+
+  validates :email, presence: true
+  validates_format_of :email, with: /.+@.+/, if: :email
+  validate :check_email, if: :email
+
+  validates :password, length: {minimum: 6, maximum: 8}, if: :password
+  validate :email_password_match, if: Proc.new { |s| s.email.present? and s.password.present? }
+
+  def check_email
+    @user ||= User.find_by_email email
+    if @user.nil?
+      errors.add :email, :not_found
+    end
+  end
+
+  def email_password_match
+    @user ||= User.find_by_email email
+    if user and not user.authenticate password
+      errors.add :password, :mismatch
+    end
+  end
+end
